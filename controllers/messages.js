62a3262291f9f4401f6f1b60632a6312d40f02d6
Ben Manley <benjamintmanley@gmail.com>
2022-06-25 23:10:45 -0500

    implement message creation functionality

---
+++ controllers/messages.js
@@ -0,0 +1,26 @@
+import { Message } from '../models/message.js'
+
+function index(req, res) {
+  Message.find({})
+  .populate('author')
+  .sort({createdAt: "asc"})
+  .then((messages) => {
+    res.render('messages/index', {
+      title: 'Message Board',
+      messages: messages.reverse()
+    })
+  })
+}
+
+function create(req, res) {
+  req.body.author = req.user.profile._id
+  Message.create(req.body)
+  .then(()=> {
+    res.redirect('/messages')
+  })
+}
+
+export { 
+  index,
+  create
+}
---
+++ routes/messages.js
@@ -0,0 +1,16 @@
+import { Router } from 'express'
+import * as messagesCtrl from "../controllers/messages.js"
+
+const router = Router()
+
+router.get('/', isLoggedIn, messagesCtrl.index)
+router.post('/', isLoggedIn, messagesCtrl.create)
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) return next();
+  res.redirect("/auth/google");
+}
+
+export {
+  router
+}
