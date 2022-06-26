c9ffb4a6203c703ec9e80e0c98aa83297f0ebf47
Ben Manley <benjamintmanley@gmail.com>
2022-06-25 22:38:22 -0500

    add game search functionality

---
+++ models/game.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema;
+
+const gameSchema = new Schema({
+  name: String,
+  rawgId: Number,
+  released: Date,
+  imageUrl: String,
+  collectedBy: [{ type: Schema.Types.ObjectId, ref: "Profile" }],
+  // To be filled in later
+  // reviews: [reference GameReview],
+},{
+  timestamps: true,
+});
+
+const Game = mongoose.model("Game", gameSchema)
+
+export {
+	Game
+}
