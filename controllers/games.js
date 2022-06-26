03d5c7601e7db24f0499fe177381c57239d57d8c
Ben Manley <benjamintmanley@gmail.com>
2022-06-25 22:57:07 -0500

    implement add/remove game to collection functionality

--- controllers/games.js
+++ controllers/games.js
@@ -15,6 +15,71 @@ function search(req, res) {
   })
 }
 
+function show(req, res) {
+  axios
+  .get(`https://api.rawg.io/api/games/${req.params.id}?key=${process.env.API_KEY}`)
+  .then((response) => {
+    Game.findOne({ rawgId: response.data.id })
+    // This is where we'll populate collectedBy
+    // This is where we'll deep-populate reviews
+    .then((game)=> {
+      res.render("games/show", {
+        title: "Game Details",
+        apiResult: response.data,
+        game,
+				userHasGame: game?.collectedBy.some(profile => profile._id.equals(req.user.profile._id)),
+      })
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/')
+  })
+}
+
+function addToCollection(req, res) {
+  req.body.collectedBy = req.user.profile._id
+  Game.findOne({ rawgId: req.params.id })
+  .then((game) => {
+    if (game) {
+      game.collectedBy.push(req.user.profile._id)
+      game.save()
+      .then(() => {
+        res.redirect(`/games/${req.params.id}`)
+      })
+    } else {
+      Game.create(req.body)
+      .then(()=> {
+        res.redirect(`/games/${req.params.id}`)
+      })
+    }
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/')
+  })
+}
+
+function removeFromCollection(req, res) {
+  // Find the game in the database
+  Game.findOne({ rawgId: req.params.id })
+  .then(game => {
+    // Remove the user's profile id from collectedBy
+    game.collectedBy.remove({_id: req.user.profile._id})
+    game.save()
+    .then(() => {
+      res.redirect(`/games/${req.params.id}`)
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/')
+  })
+}
+
 export {
-  search
+  search,
+  show,
+  addToCollection,
+  removeFromCollection
 }
